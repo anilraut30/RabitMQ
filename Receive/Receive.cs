740cd8353c55d97a23ad63f8ee39e56f87e8639b
Anil Raut <araut@badgersportswear.com>
2018-09-28 20:13:45 -0400

    Hello world working

---
+++ Receive/Receive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+           var factory = new ConnectionFactory() { HostName="localhost",Port= 5672 };
+           using(var connection = factory.CreateConnection())
+           {
+               using(var channel = connection.CreateModel())
+               {
+                   channel.QueueDeclare(queue:"hello",durable:false,exclusive:false,autoDelete:false,arguments:null);
+
+                   var consumer = new EventingBasicConsumer(channel);
+
+                   consumer.Received+= (ModuleHandle,ea) =>
+                   {
+                       var body = ea.Body;
+                       var message = Encoding.UTF8.GetString(body);
+                       Console.WriteLine($" [x] Reeived {message}");
+                   };
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit");
+                    Console.ReadLine();
+               };
+           } 
+        }
+    }
+}
